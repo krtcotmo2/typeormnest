82a99bdd4bc3d4763608904281a418e75242134f
Kurt Cooney <KCooney@interdev.com>
2023-03-03 21:17:26 -0500

    commented out AppDataSource

--- src/character/character.service.ts
+++ src/character/character.service.ts
@@ -1,7 +1,7 @@
 import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { NotFoundError } from 'rxjs';
-import { AppDataSource } from 'src/app-data-source';
+// import { NotFoundError } from 'rxjs';
+// import { AppDataSource } from 'src/app-data-source';
 import { Repository } from 'typeorm';
 import { Characters } from './characters.entity';
 
@@ -9,14 +9,14 @@ import { Characters } from './characters.entity';
 @Injectable()
 export class CharacterService {
   constructor( @InjectRepository(Characters) private repo: Repository<Characters>){}
-  ds = AppDataSource.initialize();
+  // ds = AppDataSource.initialize();
 
   async getCharacter(id:string) {
-    const char =  await AppDataSource.manager.findOneBy(Characters, {charID: +id});
-    if(!char){
-      throw new NotFoundException("Character Not Found");
-    }
-    return char;
+    // const char =  await AppDataSource.manager.findOneBy(Characters, {charID: +id});
+    // if(!char){
+    //   throw new NotFoundException("Character Not Found");
+    // }
+    return 'A character';
   }
   getEnv(){
     return {
