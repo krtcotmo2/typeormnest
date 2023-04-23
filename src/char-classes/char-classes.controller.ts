b0d278fc44640fa3d6c514199bf71b4e681cde01
Kurt Cooney <KCooney@interdev.com>
2023-04-22 23:53:01 -0400

    multi class hits and saves

--- src/char-classes/char-classes.controller.ts
+++ src/char-classes/char-classes.controller.ts
@@ -3,12 +3,12 @@ import { CharClassesService } from './char-classes.service';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { CharClassesDTO } from './dto/char-classes-dto';
 import { Observable, switchMap, of } from 'rxjs';
+import { CharClasses } from './char-classes.entity';
 
 @Controller('api/char-classes')
 export class CharClassesController {
     constructor(private charClassesService: CharClassesService){}
 
-   
     @Serialize(CharClassesDTO) 
     @Get('/?')
     getClassesForLimited(@Query('arr') query){
@@ -16,6 +16,38 @@ export class CharClassesController {
             return this.charClassesService.getClassesForLimited(query);
         }
         return this.charClassesService.getClassCharts();
+    }
+
+    @Get('/:classID/:level')
+    getStatsForClass(@Param('classID') classID: string, @Param('level') level:string){
+        return this.charClassesService.getClassesForLimited(classID).pipe(
+            switchMap((stats: CharClassesDTO[]) => {
+                return of(
+                    stats.map((stat, i) => {
+                        const lvl = level.split(',')[i];
+                        const curClassID = classID.split(',')[i];
+                        const cls = stats.find( cl => cl.classID === +curClassID);
+                        return {
+                            class: cls.className,
+                            level: +lvl,
+                            toHit: Math.ceil(
+                                cls.toHitBase + ((cls.toHitPercentage/100) * (+lvl - cls.toHitOffset))
+                            ),
+                            fort: Math.ceil(
+                                cls.fortBase + ((cls.fortPercentage/100) * (+lvl  - cls.fortOffset))
+                            ),
+                            reflex: Math.ceil(
+                                cls.reflexBase + ((cls.reflexPercentage/100) * (+lvl  - cls.reflexOffset))
+                            ),
+                            will: Math.ceil(
+                                cls.willBase + ((cls.willPercentage/100) * (+lvl  - cls.willOffset))
+                            )
+                        }
+                    })
+                    
+                )
+            })
+        );
     }
 
 }
--- src/char-classes/char-classes.entity.ts
+++ src/char-classes/char-classes.entity.ts
@@ -1,4 +1,4 @@
-import { IsBoolean, IsDate, IsInt, IsString } from "class-validator";
+import { IsBoolean, IsDate, IsDecimal, IsInt, IsNumber, IsString } from "class-validator";
 import { Column, Entity, PrimaryColumn, Table } from "typeorm";
 
 @Entity({name: 'classes'})
@@ -6,71 +6,54 @@ export class CharClasses{
     public static tableName = 'classes';
     
     @PrimaryColumn()
-    @IsInt()
     classID: number;
 
     @Column()
-    @IsString()
     className: string;
 
     @Column()
-    @IsInt()
     classHD: number;
 
     @Column()
-    @IsInt()
     toHitBase: number;
 
     @Column()
-    @IsInt()
     toHitPercentage: number;
 
     @Column()
-    @IsInt()
     toHitOffset: number;
 
     @Column()
-    @IsInt()
     fortBase: number;
 
     @Column()
-    @IsInt()
     fortPercentage: number;
 
     @Column()
-    @IsInt()
     fortOffset: number;
 
     
     @Column()
-    @IsInt()
     reflexBase: number;
 
     @Column()
-    @IsInt()
     reflexPercentage: number;
 
     @Column()
-    @IsInt()
     reflexOffset: number;
 
     @Column()
-    @IsInt()
     willBase: number;
 
     @Column()
-    @IsInt()
     willPercentage: number;
 
     @Column()
-    @IsInt()
     willOffset: number;
 
     @Column()
-    @IsDate()
     createdAt: Date;
 
     @Column()
-    @IsDate()
     updatedAt: Date;
 }
